e662f83477cae7648a278f21f15065d8078ce76d
Muhammad Hossam <40317898+MuhammadHossam@users.noreply.github.com>
2020-05-06 15:27:35 +0200

    Add GPIO Driver

---
+++ Atmega32_Drivers/GPIO/GPIO.c
@@ -0,0 +1,182 @@
+/*
+ * GPIO.c
+ *
+ *  Created on: Apr 25, 2020
+ *      Author: Muhammad Hossam
+ */
+
+#include "GPIO.h"
+
+
+volatile unsigned char* GPIO_ports[] = {GPIOA_PORT,GPIOB_PORT,GPIOC_PORT,GPIOD_PORT};
+volatile unsigned char* GPIO_dirs[] =  {GPIOA_DDR,GPIOB_DDR,GPIOC_DDR,GPIOD_DDR};
+volatile unsigned char* GPIO_pins[] =  {GPIOA_PIN,GPIOB_PIN,GPIOC_PIN,GPIOD_PIN};
+
+u8 get_port_index (u8 port_name)
+{
+	u8 i;
+	switch (port_name)
+	{
+	case 'A':
+	case 'a':
+		i = 0;
+		break;
+	case 'B':
+	case 'b':
+		i = 1;
+		break;
+	case 'C':
+	case 'c':
+		i = 2;
+		break;
+	case 'D':
+	case 'd':
+		i = 3;
+		break;
+	}
+	return i;
+}
+
+
+void GPIO_voidInit(void)
+{
+	/*comment!: DDR initialization */
+	*GPIO_dirs[0] = Init_direction_default;
+	*GPIO_dirs[1] = Init_direction_default;
+	*GPIO_dirs[2] = Init_direction_default;
+	*GPIO_dirs[3] = Init_direction_default;
+}
+
+
+/*comment! : Read Pin Value */
+u8 GPIO_u8_ReadPinVal(u8 PinIdx,u8* PtrToVal,u8 Port_Name)
+{
+	u8 API_Status;
+	if(PinIdx > no_of_pins_on_port - 1)
+	{
+		API_Status = error_happen;
+	}
+	else
+	{
+		*PtrToVal = READBIT(*GPIO_pins[get_port_index(Port_Name)],PinIdx);
+		API_Status = No_error;
+	}
+	return API_Status;
+}
+/*Comment!: Read Pin Value Long Pressed*/
+u8 GPIO_u8_ReadPinVal_LONG_PRESS(u8 PinIdx,u8* PtrToVal,u8 Port_Name)
+{
+	u8 API_Status;
+	if(PinIdx > no_of_pins_on_port - 1)
+	{
+	   API_Status = error_happen;
+	}
+	else
+	{
+		*PtrToVal = READBIT(*GPIO_pins[get_port_index(Port_Name)],PinIdx);
+	#ifdef LONG_PRESS
+		if(*PtrToVal == 1)
+		{
+			while(READBIT(*GPIO_pins[get_port_index(Port_Name)],PinIdx));
+			API_Status = No_error;
+		}
+ 	 #endif
+	}
+	return API_Status;
+
+}
+
+/*Comment! : Write Pin Value */
+u8 GPIO_u8_WritePinVal(u8 PinIdx,u8 PinVal, u8 Port_Name)
+{
+	u8 API_Status;
+	if(PinIdx > no_of_pins_on_port - 1)
+	{
+		API_Status = error_happen;
+	}
+	else
+	{
+		switch(PinVal)
+		{
+		case 1:
+			SETBIT(*GPIO_ports[get_port_index(Port_Name)],PinIdx);
+			API_Status = No_error;
+			break;
+		case 0:
+			CLRBIT(*GPIO_ports[get_port_index(Port_Name)],PinIdx);
+			API_Status = No_error;
+			break;
+		}
+	}
+
+	return API_Status;
+}
+
+/*Comment!: Write Pin Direction */
+u8 GPIO_u8_WritePinDir(u8 PinIdx,u8 PinDir, u8 Port_Name)
+{
+	u8 API_Status;
+	if(PinIdx > no_of_pins_on_port - 1)
+	{
+		API_Status = error_happen;
+	}
+	else
+	{
+		switch(PinDir)
+		{
+		case 1:
+			SETBIT(*GPIO_dirs[get_port_index(Port_Name)],PinIdx);
+			API_Status = No_error;
+			break;
+		case 0:
+			CLRBIT(*GPIO_dirs[get_port_index(Port_Name)],PinIdx);
+			API_Status = No_error;
+			break;
+		}
+	}
+	return API_Status;
+}
+
+/*Comment!: Read Pin Direction */
+u8 GPIO_u8_ReadPinDir(u8 PinIdx,u8* PtrToDir,u8 Port_Name)
+{
+	u8 API_Status;
+	if(PinIdx > no_of_pins_on_port - 1)
+	{
+		API_Status = error_happen;
+	}
+	else
+	{
+		*PtrToDir = READBIT(*GPIO_dirs[get_port_index(Port_Name)],PinIdx);
+		API_Status = No_error;
+	}
+	return API_Status;
+}
+
+/*Comment!: Read Port Value */
+u8 GPIO_u8_ReadPortVal(u8* PtrToVal, u8 Port_Name)
+{
+	*PtrToVal = *GPIO_pins[get_port_index(Port_Name)];
+	return No_error;
+}
+
+/*Comment!: Write Port Value */
+u8 GPIO_u8_WritePortVal(u8 PortVal, u8 Port_Name)
+{
+	*GPIO_ports[get_port_index(Port_Name)] = PortVal;
+	return No_error;
+}
+
+/*Comment!: Write Port Direction*/
+u8 GPIO_u8_WritePortDir(u8 PortDir, u8 Port_Name)
+{
+	*GPIO_dirs[get_port_index(Port_Name)] = PortDir;
+	return No_error;
+}
+
+/*Comment!: Read Port Direction*/
+u8 GPIO_u8_ReadPortDir(u8* PtrToDir, u8 Port_Name)
+{
+	*PtrToDir = *GPIO_dirs[get_port_index(Port_Name)];
+	return No_error;
+}
---
+++ Atmega32_Drivers/GPIO/GPIO.h
@@ -0,0 +1,65 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: Apr 25, 2020
+ *      Author: Muhammad Hossam
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+#include "DATA_Types.h"
+#include "ATMEGA32_registers.h"
+#include "std_macros.h"
+
+#define u8_Direction_input 0x00
+#define u8_Direction_output 0xFF
+
+// choose the default initialization for GPIO
+// initialize the pins by zeros or ones
+
+#define Init_direction_default u8_Direction_input
+#define no_of_pins_on_port 8
+
+//=====================Initialization=======================
+/*comment! : Initialization Function */
+void GPIO_voidInit(void);
+//==========================================================
+
+//======================PIN=================================
+//Data
+/*comment!: Read Pin Value */
+extern u8 GPIO_u8_ReadPinVal(u8 PinIdx,u8* PtrToVal,u8 Port_Name);
+
+/*comment!: Read Pin Value Long Pressed*/
+extern u8 GPIO_u8_ReadPinVal_LONG_PRESS(u8 PinIdx,u8* PtrToVal,u8 Port_Name);
+
+/*Comment!: Write Pin Value */
+extern u8 GPIO_u8_WritePinVal(u8 PinIdx,u8 PinVal, u8 Port_Name);
+
+//Direction
+/*Comment!: Write Pin Direction */
+extern u8 GPIO_u8_WritePinDir(u8 PinIdx,u8 PinDir, u8 Port_Name);
+
+/*comment!: Read Pin Direction */
+extern u8 GPIO_u8_ReadPinDir(u8 PinIdx,u8* PtrToDir,u8 Port_Name);
+
+//========================PORT===============================
+//Data
+/*Comment! : Read Port Value*/
+extern u8 GPIO_u8_ReadPortVal(u8* PtrToVal, u8 Port_Name);
+
+/*Comment! : Write Port Value*/
+extern u8 GPIO_u8_WritePortVal(u8 PortVal, u8 Port_Name);
+
+//Direction
+/*Comment! : Write Port Direction*/
+extern u8 GPIO_u8_WritePortDir(u8 PortDir, u8 Port_Name);
+
+/*Comment! : Read Port Direction*/
+extern u8 GPIO_u8_ReadPortDir(u8* PtrToDir, u8 Port_Name);
+
+/*Comment! : Get Port index*/
+extern u8 get_port_index (u8 port_name);
+
+#define LONG_PRESS
+#endif /* GPIO_H_ */
